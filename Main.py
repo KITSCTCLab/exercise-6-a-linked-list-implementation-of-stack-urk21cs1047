91d1225a9a965593c79b0bbef1dc0dab3a0238ce
urk21cs1047 <109952057+urk21cs1047@users.noreply.github.com>
2022-09-10 21:38:04 +0530

    initial commit

--- Main.py
+++ Main.py
@@ -1,47 +1,32 @@
 class Node:
-  def _init_(self, data):
+  def __init__(self, data):
     self.data = data
     self.next = None
-#     self.top=None
 
 class Stack:
-  def _init_(self):
-    self.top = None
-    #self.top=None
+  def __init__(self):
+    self.head = None
+
   def push(self, data) -> None:
-    # Write your code here
-    new=Node(data)
-    new.data=data
-    new.next=self.top
-    self.top=new
+    nn = Node(data)
+    nn.next = self.head
+    self.head = nn
+
   def pop(self) -> None:
-    # Write your code here
-    if self.top==None:
-      return None
-    else:
-      #temp=Node(data)
-      temp=self.top
-      self.top=self.top.next
-      temp.next=None
-      return temp
-      
-      
+    if(self.head!=None):
+      temp = self.head
+      self.head = temp.next
   def status(self):
-    """
-    It prints all the elements of stack.
-    """
-    # Write your code here  
-    if self.top==None:
-      print("None")
-    else:
-      #ptr=Node(data)
-      ptr=self.top
-      while(ptr!=None):
-        print(ptr.data,end="")
-        print("=>",end="")
-        ptr=ptr.next
-        if(ptr==None):
-          print("None")
+    if(self.head  != None):
+      temp = self.head
+      while temp.next!= None:
+        print(temp.data, end = "")
+        print("=>", end = "")
+        temp = temp.next
+      print(temp.data, end = "=>")
+      
+    print("None")
+
 # Do not change the following code
 stack = Stack()
 operations = []
